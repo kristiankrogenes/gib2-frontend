ba23398e92ce0ec997d6b6473057eecf35df79ab
Kristian Krøgenes <kristiankrogenes@gmail.com>
2022-02-08 15:13:47 +0100

    plz

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Map, { Marker } from 'react-map-gl';
+// import Map, { Marker } from 'react-map-gl';
 import { Box } from '@material-ui/core';
  
-const MAPBOX_TOKEN="pk.eyJ1Ijoic2lndXJkYmFra2VydWQiLCJhIjoiY2t6Y2ZpYjJtMGhxcDJ4cDh6NTdobXp0dyJ9.gxwAMoaOj91JvNVzVwl6Hg";
+// const MAPBOX_TOKEN="pk.eyJ1Ijoic2lndXJkYmFra2VydWQiLCJhIjoiY2t6Y2ZpYjJtMGhxcDJ4cDh6NTdobXp0dyJ9.gxwAMoaOj91JvNVzVwl6Hg";
 
 function MapComponent() {
 
     // coordinates for lerkendalsbygget
-    const lerka = {longitude: 10.406852960586548, latitude: 63.4147798358434}
+    // const lerka = {longitude: 10.406852960586548, latitude: 63.4147798358434}
 
-    const initialViewState = {
-            longitude: lerka.longitude,
-            latitude: lerka.latitude,
-            zoom: 14,
-            width: "100%",
-            height: "100%"
-        };
+    // const initialViewState = {
+    //         longitude: lerka.longitude,
+    //         latitude: lerka.latitude,
+    //         zoom: 14,
+    //         width: "100%",
+    //         height: "100%"
+    //     };
 
     return (
         <Box sx={{
