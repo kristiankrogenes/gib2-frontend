4c8ec8e0e61838608e2faf4b29b45dd277fed28b
Kristian Krøgenes <kristiankrogenes@gmail.com>
2022-02-22 18:03:58 +0100

    auth create and login

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import SignUpDialog from "../components/authentication/SignUpDialog";
+
+export default function RegisterPage() {
+  return <SignUpDialog />;
+}
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,8 @@ import Products from "./pages/Products";
 import Blog from "./pages/Blog";
 import User from "./pages/User";
 import NotFound from "./pages/Page404";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 // ----------------------------------------------------------------------
 
@@ -23,6 +25,8 @@ export default function Router() {
         { path: "user", element: <User /> },
         { path: "products", element: <Products /> },
         { path: "blog", element: <Blog /> },
+        { path: "register", element: <RegisterPage /> },
+        { path: "login", element: <LoginPage /> },
       ],
     },
     {
