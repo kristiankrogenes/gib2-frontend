9f558360b0efd0798f7abb5019e9633684d6d9ba
Sigurd Bakkerud <sigurbak@stud.ntnu.no>
2022-03-08 22:50:25 +0100

    Format code

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { filter } from 'lodash';
 import { Icon } from '@iconify/react';
 import { sentenceCase } from 'change-case';
